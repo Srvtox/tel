2a4a141a536513eaa4cf05ac6caedad276d72362
Srvtox <mr.nobody.cxyz@gmail.com>
2026-04-27 16:43:21 +0330

    Update sas.sh

--- sas.sh
+++ sas.sh
@@ -8,13 +8,13 @@ echo "======================================"
 sudo apt-get update -y
 
 sudo apt-get install -y \
-xvfb \
-xfce4 \
-xfce4-terminal \
-dbus-x11 \
-ffmpeg \
-x11vnc \
-wget
+  xvfb \
+  xfce4 \
+  xfce4-terminal \
+  dbus-x11 \
+  ffmpeg \
+  x11vnc \
+  wget -y
 
 echo "======================================"
 echo "Starting virtual display"
@@ -33,25 +33,24 @@ startxfce4 &
 sleep 8
 
 echo "======================================"
-echo "Starting screen streaming to Aparat"
+echo "Starting screen streaming to Aparat (VIDEO ONLY)"
 echo "======================================"
 
 STREAM_KEY="ef6d74868f69db63a3c11a33a8f899d8f?s=54d20ca45fba421a"
+RTMP_URL="rtmp://rtmp.cdn.asset.aparat.com:443/event/$STREAM_KEY"
+
+echo "RTMP URL: $RTMP_URL"
 
 ffmpeg \
--f x11grab \
--video_size 1280x720 \
--framerate 30 \
--i :99.0 \
--f pulse -ac 2 -i default \
--c:v libx264 \
--preset veryfast \
--b:v 2500k \
--maxrate 2500k \
--bufsize 5000k \
--pix_fmt yuv420p \
--g 60 \
--c:a aac \
--b:a 128k \
--f flv \
-rtmp://rtmp.cdn.asset.aparat.com:443/event/$STREAM_KEY
+  -f x11grab \
+  -video_size 1280x720 \
+  -framerate 30 \
+  -i :99.0 \
+  -c:v libx264 \
+  -preset veryfast \
+  -b:v 2500k \
+  -maxrate 2500k \
+  -bufsize 5000k \
+  -pix_fmt yuv420p \
+  -g 60 \
+  -f flv "$RTMP_URL"
